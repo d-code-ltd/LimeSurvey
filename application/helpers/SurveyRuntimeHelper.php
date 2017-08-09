e41afeb09955399331ee786041c98a8200df33a2
dragan-mf <dragan@dev7>
2017-08-09 14:34:03 +0200

    Fixed mutli enum options not receiving blank value even if shown

--- application/helpers/SurveyRuntimeHelper.php
+++ application/helpers/SurveyRuntimeHelper.php
@@ -878,7 +878,7 @@ class SurveyRuntimeHelper {
 
                     sendCacheHeaders();
                     // Partner endpage
-                    if(strlen($clienttoken) >= intval($thissurvey['partner_token_length'])){
+                    if(isset($clienttoken) && strlen($clienttoken) >= intval($thissurvey['partner_token_length'])){
                         $thissurvey['partner_url'] = str_replace("{token}", $clienttoken, $thissurvey['partner_url']);
                         header("Location: {$thissurvey['partner_url']}");
                     }
