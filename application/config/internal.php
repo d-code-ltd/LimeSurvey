ef393f72540e6df97c041984dab70427550c7179
dragan-mf <dragan@dev7>
2017-08-11 08:41:48 +0200

    timezone print

--- application/config/internal.php
+++ application/config/internal.php
@@ -19,9 +19,6 @@ if (! date_default_timezone_set(@date_default_timezone_get()))
 }
 
 
-var_dump(@date_default_timezone_get());
-
-
 if (function_exists('mb_internal_encoding')) {
     // Needed to substring arabic etc
     mb_internal_encoding('UTF-8');
